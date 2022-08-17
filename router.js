189f8bd65d163a4a0219646c9b1d7a9010f1252c
lyonqingmango <qingmango@gmail.com>
2022-08-17 15:17:17 -0700

    add routers

--- router.js
+++ router.js
@@ -2,9 +2,34 @@ const express = require('express');
 const router = express.Router();
 const{Photos, Answers, Questions} =require('./db.js')
 
+router.get('/qa/questions',(req,res)=>{
+  res.send('Get all questions')
 
-router.get('/getAll', (req, res) => {
-  res.send('Get All API')
 })
+router.get('/qa/questions/:question_id/answers', (req, res) => {
+  res.send('Get All answers')
+})
+
+
+//GET /qa/questions/:question_id/answers
+//page count question_id
+// app.get('/answers/:qId', (req, res) => {
+//   apiReq({
+//     url: `${BASEURL}/qa/questions/${req.params.qId}/answers`,
+//     headers: options.headers,
+//     params: req.query
+//   }, (err, data) => {
+//     err ? res.sendStatus(500) : res.json(data);
+//   });
+// })//product_id page count
+// //app.get('/questions', (req, res) => {
+//   apiReq({
+//     url: `${BASEURL}/qa/questions`,
+//     headers: options.headers,
+//     params: req.query
+//   }, (err, data) => {
+//     err ? res.sendStatus(500) : res.json(data);
+//   });
+// });
 
 module.exports = router
