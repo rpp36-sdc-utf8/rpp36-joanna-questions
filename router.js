53d21ccec0ddf79eeae911963d78a6a378a5bb91
lyonqingmango <qingmango@gmail.com>
2022-08-20 15:44:54 -0700

    add router get questions

--- router.js
+++ router.js
@@ -3,7 +3,75 @@ const router = express.Router();
 const{Photos, Answers, Questions} =require('./db.js')
 
 router.get('/qa/questions',(req,res)=>{
-  res.send('Get all questions')
+  var product_id,page,count =req.params.product_id;
+  var page =req.params.page || 1;
+  var count = req.params.count|| 5;
+  Questions
+  .aggregate([
+    {
+      '$match': {
+        'product_id': 1
+      }
+    }, {
+      '$skip': 0
+    }, {
+      '$limit': 5
+    }, {
+      '$lookup': {
+        'from': 'answers',
+        'localField': 'id',
+        'foreignField': 'question_id',
+        'as': 'answers'
+      }
+    }, {
+      '$project': {
+        '_id': 0,
+        'question_id': '$id',
+        'product_id': 1,
+        'date_written': 1,
+        'body': 1,
+        'asker_name': 1,
+        'reported': 1,
+        'question_helpfulness': '$helpful',
+        'answers': {
+          '$arrayToObject': {
+            '$map': {
+              'input': '$answers',
+              'in': {
+                'k': {
+                  '$ifNull': [
+                    {
+                      '$toString': '$$this.id'
+                    }, 'replacement-id'
+                  ]
+                },
+                'v': '$$this'
+              }
+            }
+          }
+        }
+      }
+    }
+  ])
+  .then(results=>{
+    var data ={};
+    var arr =[];
+    data.product_id = results[0].product_id;
+    data.results=arr;
+    results.forEach(result=>{
+      result.question_date = new Date(result.date_written)
+      arr.push(result)
+    })
+
+
+    console.log('succes inside questions '),res.json(data)})
+  .catch(err=>res.status(500).send('err inside get questions'))
+
+
+
+
+
+  // res.send('Get all questions')
 
 })
 router.get('/qa/questions/:question_id/answers', (req, res) => {
@@ -21,13 +89,6 @@ router.get('/qa/questions/:question_id/answers', (req, res) => {
       '$skip': (page-1)*count
       }, {
       '$limit': count
-      }, {
-      '$lookup': {
-        'from': 'photos',
-        'localField': 'id',
-        'foreignField': 'answer_id',
-        'as': 'photos'
-      }
       }
     ])
     .then((results)=>{
