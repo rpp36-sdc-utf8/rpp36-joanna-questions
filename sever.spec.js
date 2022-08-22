df2ca3633d5ec61a16047bc452a9f8c2917f679e
lyonqingmango <qingmango@gmail.com>
2022-08-21 23:50:16 -0700

    get test success for questions and answers

--- sever.spec.js
+++ sever.spec.js
@@ -1,56 +1,36 @@
 
-const app = require("./server");
-const supertest = require("supertest");
-const request = supertest(app);
-const {MongoClient} = require('mongodb');
+const app = require('./server') // Link to your server file
+const supertest = require('supertest')
+const request = supertest(app)
+const mongoose = require("mongoose");
+const databaseName = "sdc";
 
 describe("GET / ", () => {
-  let connection;
-  let db;
 
-  beforeAll(async () => {
-    connection = await MongoClient.connect('mongodb://localhost:27017/sdc', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    db = await connection.db(globalThis.sdc);
-
-
-  })
-  afterAll(async () => {
-    await connection.close();
-  });
-
-  it("It should respond", async (done) => {
-    const response = await request(app).get("/");
+  it("It should respond", async () => {
+    const response = await request.get("/");
     expect(response.statusCode).toBe(200);
-    done()
   });
-  it("get responses correctly in get questions", async (done)=>{
-
-    const res = await request(app)
-    .get('/qa/questions/1/answers');
-    expect(res.statusCode).toBe(200);
-    done()
-    })
 
 });
 
 
-// describe('Get /qa/questions',()=>{
-//   test("get responses correctly in get questions", async ()=>{
+describe('Get /qa/questions/:qId/answers',()=>{
 
-//   const res = await request(app).get('/qa/questions');
-//   expect(res.statusCode).tobe(200);
+  it("get responses correctly in get answers", async()=>{
 
+    const res = await request.get('/api/qa/questions/1/answers');
+    expect(res.status).toBe(200);
 
-//   // expect(res.body[0]).toHaveProperty('title');
-//   // expect(res.body[0].title).toBe('Alternate Endings for Game of Thrones, Season 8');
+  })
+})
 
-//   // expect(res.body[0]).toHaveProperty('author');
-//   // expect(res.body[0].author).toBe('Literally Anyone');
+describe('Get /qa/questions',()=>{
 
-//   // expect(res.body[0]).toHaveProperty('publisher');
-//   // expect(res.body[0].publisher).toBe('Not George R. R. Martin');
-//   })
-// })
+  it("get responses correctly in get questions", async()=>{
+
+    const res = await request.get('/api/qa/questions/?product_id=1&page=1&count=2&search=');
+    expect(res.status).toBe(200);
+
+  })
+})
