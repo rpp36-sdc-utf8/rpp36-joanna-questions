63fcda684a091b9604c65244eb28f939d068b43b
Joanna Zhang <31421459+lyonqingmango@users.noreply.github.com>
2023-02-13 22:43:48 -0800

    Update router.js

--- router.js
+++ router.js
@@ -207,16 +207,7 @@ router.put('/qa/answers/:answer_id/helpful',async(req,res)=>{
   }catch(err){
     res.status(500).send('err inside helpful exec answers');console.log(err)
   }
-  // Answers.updateOne({id:answer_id},{$inc:{'answers.helpful':1}})
-  // .exec((err,result)=>{
-  //   if(err){
-  //     res.status(500).send('err inside helpful exec answers');console.log(err)
-  //   }else{
-  //     res.status(204).send();console.log('success helpful questions')
-  //   }
-  // })
-
-// put question helpful
+  
 
 })
 router.put('/qa/questions/:question_id/helpful',(req,res)=>{
@@ -273,30 +264,7 @@ router.post('/qa/questions/:question_id/answers',async(req,res)=>{
 
         })
     }else{
-    //   var answerResult = await Answers.find({}).sort({answer_id:-1}).limit(1).exec()
-    //   var answer_id = answerResult[0].answer_id+1
-    //   // console.log('answer_id'+answer_id)
-    //   var photosResult = await Photos.find({}).sort({id:-1}).limit(1).exec()
-    //   var photoId = photosResult[0].id+1
-    //   // console.log('photoId'+ photoId)
-    //   photos = photos.map((photo) => {
-    //     photoId = photoId+1
-    //     // console.log('photoID'+photoId)
-    //     return {
-    //     url: photo,
-    //     id:photoId,
-    //     answer_id:answer_id
-    //   };
-    // })
-    //   var photoData = await Photos.insertMany(photos)
-    //   console.log(photoData)
-    //   photoData = photoData.map((photo) => {
-    //     return {
-    //     url: photo.url,
-    //     id:photo.id
-    //   };
-    // })
-
+    
       Answers.create({question_id:question_id,body:body,
         answerer_email:answerer_email,answerer_name:answerer_name,photos:photos, date: new Date().getTime()},function (err, result) {
           if (err) {
@@ -325,118 +293,3 @@ module.exports = router
 
 
 
-// [
-//   {
-//     '$project': {
-//       '_id': 1,
-//       'id': 1,
-//       'product_id': 1,
-//       'body': 1,
-//       'question_date': '$date_written',
-//       'asker_email': 1,
-//       'asker_name': 1,
-//       'reported': 1,
-//       'question_helpfulness': 1
-//     }
-//   }, {
-//     '$merge': {
-//       'into': 'questions',
-//       'on': '_id',
-//       'whenMatched': 'replace'
-//     }
-//   }
-// ]
-
-// {
-//   "_id": {
-//     "$oid": "62fb3a95167367696a233de1"
-//   },
-//   "id": 2,
-//   "product_id": 1,
-//   "body": "HEY THIS IS A WEIRD QUESTION!!!!?",
-//   "asker_name": "jbilas",
-//   "asker_email": "first.last@gmail.com",
-//   "reported": true,
-//   "question_helpfulness": 4,
-//   "question_date": "1613888219613"
-// }
-//{
-//   "_id": {
-//     "$oid": "62fb3b13167367696a3e673a"
-//   },
-//   "id": 1780059,
-//   "product_id": 506121,
-//   "body": "Illo enim quae enim voluptates explicabo sit nemo.",
-//   "date_written": "1614580519007",
-//   "asker_name": "Lucienne_Ernser",
-//   "asker_email": "Candido5@yahoo.com",
-//   "reported": true,
-//   "question_helpfulness": 28
-// }
-// //{
-//   "_id": {
-//     "$oid": "62fdd80e7aa3d2308c401977"
-//   },
-//   "id": 3476787,
-//   "question_id": 1780060,
-//   "body": "Vel nihil maxime eligendi et voluptate.",
-//   "date_written": 1617933132000,
-//   "answerer_name": "Delaney41",
-//   "answerer_email": "Seller",
-//   "reported": false,
-//   "helpful": 13
-// }
-
-// //{
-//   "_id": {
-//     "$oid": "63102e478a07b61cd8c162c6"
-//   },
-//   "id": 6879308,
-//   "body": "hellotestbody",
-//   "date": 1662004807114,
-//   "question_id": 1,
-//   "answerer_name": "helltestame",
-//   "answerer_email": "testemail@gmail.com",
-//   "helpfulness": 0,
-//   "reported": false,
-//   "photos": [
-//     {
-//       "id": 2063761,
-//       "url": "http:2313",
-//       "_id": {
-//         "$oid": "63102e478a07b61cd8c162c7"
-//       }
-//     },
-//     {
-//       "id": 2063762,
-//       "url": "http:5532",
-//       "_id": {
-//         "$oid": "63102e478a07b61cd8c162c8"
-//       }
-//     }
-//   ],
-//   "__v": 0
-// }
-
-// [
-//   {
-//     '$lookup': {
-//       'from': 'photos',
-//       'localField': 'id',
-//       'foreignField': 'answer_id',
-//       'as': 'photos'
-//     }
-//   }, {
-//     '$project': {
-//       'photos': {
-//         'answer_id': 0
-//       }
-//     }
-//   }, {
-//     '$merge': {
-//       'into': 'answers',
-//       'on': '_id',
-//       'whenMatched': 'replace'
-//     }
-//   }
-// ]
